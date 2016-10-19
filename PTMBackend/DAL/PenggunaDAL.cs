1f370da1f0944dc08e44bba58396746f39fdc93d
erick <erick@DESKTOP-7DP9Q9N>
2016-10-19 11:37:25 +0700

    menambahkan PenggunaDAL dan method GetLogin

--- PTMBackend/DAL/PenggunaDAL.cs
+++ PTMBackend/DAL/PenggunaDAL.cs
@@ -4,10 +4,34 @@ using System.Linq;
 using System.Web;
 
 using PTMBackend.Models;
+using System.Data.SqlClient;
+using System.Configuration;
+using Dapper;
 
 namespace PTMBackend.DAL
 {
     public class PenggunaDAL
     {
+        private string GetConnString()
+        {
+            return ConfigurationManager.ConnectionStrings["PTMDbConnectionString"].ConnectionString;
+        }
+
+        public Pengguna GetLogin(string username,string password)
+        {
+            using (SqlConnection conn = new SqlConnection(GetConnString()))
+            {
+                string strSql = @"select * from Pengguna left join TipeUser
+                                  on Pengguna.IdTipeUser=TipeUser.IdTipeUser
+                                  where Pengguna.Username=@Username and Pengguna.Password=@Password";
+
+                var pengguna = conn.Query<Pengguna, TipeUser, Pengguna>(strSql, (p, t) => {
+                    p.TipeUser = t;
+                    return p;
+                }, new { Username = username, Password = password }, splitOn: "IdTipeUser");
+
+                return pengguna.FirstOrDefault();
+            }
+        } 
     }
 }
