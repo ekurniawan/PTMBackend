f407c63b0fbb3510fad52f709b2bd84ac83b162f
erick <erick@DESKTOP-7DP9Q9N>
2016-10-19 11:13:12 +0700

    menambahkan connection string

---
+++ PTMBackend/DAL/PenggunaDAL.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using PTMBackend.Models;
+
+namespace PTMBackend.DAL
+{
+    public class PenggunaDAL
+    {
+    }
+}
