94e95a1991539a20801e44828568458bc8a9b721
erick <erick@DESKTOP-7DP9Q9N>
2016-10-19 11:00:16 +0700

    menambahkan Business Object

---
+++ PTMBackend/Models/BusinessObject.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PTMBackend.Models
+{
+    public class COIDetail
+    {
+        public string COINumber { get; set; }
+        public string PolNumber { get; set; }
+        public int IdTipeStatus { get; set; }
+        public string DestinationPort { get; set; }
+        public string VesselName { get; set; }
+
+        public TipeStatus TipeStatus { get; set; }
+    }
+
+    public class Pengguna
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public int IdTipeUser { get; set; }
+
+        public TipeUser TipeUser { get; set; }
+    }
+
+    public class TipeStatus
+    {
+        public TipeStatus()
+        {
+            this.CoiDetails = new List<COIDetail>();
+        }
+
+        public int IdTipeStatus { get; set; }
+        public string NamaTipe { get; set; }
+
+        public List<COIDetail> CoiDetails { get; set; }
+    }
+
+    public class TipeUser
+    {
+        public TipeUser()
+        {
+            this.Penggunas = new List<Pengguna>();
+        }
+
+        public int IdTipeUser { get; set; }
+        public string NamaTipe { get; set; }
+        public bool IsConfirm { get; set; }
+        public bool IsRequest { get; set; }
+        public bool IsReview { get; set; }
+        public bool IsApprove { get; set; }
+        public bool IsAccept { get; set; }
+
+        public List<Pengguna> Penggunas { get; set; }
+    }
+
+}
